6390f90d3e60dc3cc3f51f40d90a513bc9086538
Gerald09 <xianfeng1991@gmail.com>
2016-11-20 23:38:04 +0800

    8 - rotate a 3D cube

--- src/com/thinmatrix/opengl3dgamedemo/Main.java
+++ src/com/thinmatrix/opengl3dgamedemo/Main.java
@@ -23,37 +23,94 @@ public class Main {
 
         // 顶点坐标
         float[] vertices = {
-                -0.5f, 0.5f, 0f, // v0
-                -0.5f, -0.5f, 0f, // v1
-                0.5f, -0.5f, 0f, // v2
-                0.5f, 0.5f, 0f, // v3
+                -0.5f, 0.5f, 0,
+                -0.5f, -0.5f, 0,
+                0.5f, -0.5f, 0,
+                0.5f, 0.5f, 0,
+
+                -0.5f, 0.5f, 1,
+                -0.5f, -0.5f, 1,
+                0.5f, -0.5f, 1,
+                0.5f, 0.5f, 1,
+
+                0.5f, 0.5f, 0,
+                0.5f, -0.5f, 0,
+                0.5f, -0.5f, 1,
+                0.5f, 0.5f, 1,
+
+                -0.5f, 0.5f, 0,
+                -0.5f, -0.5f, 0,
+                -0.5f, -0.5f, 1,
+                -0.5f, 0.5f, 1,
+
+                -0.5f, 0.5f, 1,
+                -0.5f, 0.5f, 0,
+                0.5f, 0.5f, 0,
+                0.5f, 0.5f, 1,
+
+                -0.5f, -0.5f, 1,
+                -0.5f, -0.5f, 0,
+                0.5f, -0.5f, 0,
+                0.5f, -0.5f, 1
+
         };
 
         // texture坐标点
         float[] textureCoords = {
-                0, 0, // v0
-                0, 1, // v1
-                1, 1, // v2
-                1, 0, // v3
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0
         };
 
         // 顶点顺序
         int[] indices = {
                 0, 1, 3,
                 3, 1, 2,
+                4, 5, 7,
+                7, 5, 6,
+                8, 9, 11,
+                11, 9, 10,
+                12, 13, 15,
+                15, 13, 14,
+                16, 17, 19,
+                19, 17, 18,
+                20, 21, 23,
+                23, 21, 22
         };
 
         RawModel model = loader.loadToVao(vertices, textureCoords, indices);
         ModelTexture texture = new ModelTexture(loader.loadTexture("dog0"));
         TextureModel textureModel = new TextureModel(model, texture);
 
-        Entity entity = new Entity(textureModel, new Vector3f(0, 0, -5f), 0, 0, 0, 1);
+        Entity entity = new Entity(textureModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
 
         Camera camera = new Camera();
 
         while (!Display.isCloseRequested()) {
 //            entity.increasePosition(0, 0, -0.0001f);
 //            entity.increaseRotation(0, 0, 360f);
+            entity.increaseRotation(0.025f, 0.025f, 0.025f);
             renderer.prepare();
             camera.move();
             staticShader.start();
