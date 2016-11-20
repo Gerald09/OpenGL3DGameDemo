5745374d1e05d29228d77eef90a32e76d47eeab2
Gerald09 <xianfeng1991@gmail.com>
2016-11-20 10:56:33 +0800

    lesson 1-init project library & display

--- src/com/thinmatrix/opengl3dgamedemo/Main.java
+++ src/com/thinmatrix/opengl3dgamedemo/Main.java
@@ -1,8 +1,19 @@
 package com.thinmatrix.opengl3dgamedemo;
 
+import com.thinmatrix.opengl3dgamedemo.renderengine.DisplayManager;
+
+import org.lwjgl.opengl.Display;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("jajahahj");
+        DisplayManager.createDisplay();
+        while (!Display.isCloseRequested()) {
+            // game logic
+            // render
+            DisplayManager.updateDisplay();
+        }
+
+        DisplayManager.closeDisplay();
     }
 }
